9c3435a30e9e8f6d773ebd95785cec2ca862efb6
navabiradar <nava526@outlook.com>
2024-08-19 11:09:30 +0530

    Update Site Map

---
+++ src/routes/sitemap/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<main>
+    <ul>
+        <li>
+            <a href="/">Karnataka State Government Employees Vedike</a>
+        </li>
+        <li>
+            <a href="/DAorders">Karnataka State Employees Dearness Allowance Orders</a>
+        </li>
+        <li>
+            <a href="/NormalPensionCalculator">Pension Calculator</a>
+        </li>
+        <li>
+            <a href="/7thPaySalaryCalculation">7th Pay Fixation Calculator</a>
+        </li>
+        <li>
+            <a href="/7thPayFamilyPensionCalculation">7th Pay Family Pension Fixation Calculator</a>
+        </li>
+        <li>
+            <a href="/7thPayPensionCalculation">7th Pay Pension Fixation Calculator</a>
+        </li>
+        <li>
+            <a href="/sitemap">sitemap</a>
+        </li>
+    </ul>
+</main>
